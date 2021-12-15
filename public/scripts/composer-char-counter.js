44672d2728d87ccc6db8f25c242d29740b15bc24
Matthew Litvinov <matthew.litvinov@gmail.com>
2021-12-14 20:11:01 -0500

    create example tweets and style

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+//keep track of character count in textArea
+$(document).ready(function(){
+  const textArea = $("#tweet-text");
+  
+  textArea.on('input', function(event) {
+    const counter = $(this).next().children('output');
+    const maxLength = 140;
+    const currentLength = $(this).val().length;
+    let charLeft = maxLength - currentLength;
+
+    //change color of counter to red if less than 0 chars
+    $(counter).css("color", "");
+    if(charLeft < 0) {
+      $(counter).css("color", "red");
+    } 
+    $(counter).text(charLeft);
+  });
+}); 
